557639308233e1318e6a6d0fdc4459109dd71ede
root <root@ip-10-212-127-8.ec2.internal>
2012-03-01 11:49:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+M::Application.config.secret_token = 'bfebbb888ab99db4fa9b70b61f0b6b8604f256a46baf225cf624d1a3021a58a8bb57b6b72aa788ec5353d206ef96ebea5de1fca391f39abbbdcbcc7fd38eb627'
